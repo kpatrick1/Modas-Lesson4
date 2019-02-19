c97a4eb6cdc67f54558cf8e7330304c95389cec1
Kevin Patrick <kevin.ptrck@yahoo.com>
2019-02-18 19:04:23 -0600

    Add project files.

---
+++ wwwroot/js/client.js
@@ -0,0 +1,17 @@
+﻿// Turn off ESLint (Windows): Tools - Options - Text Editor - Javascript - Linting
+$(function () {
+    getEvents(1)
+
+    function getEvents(page) {
+        $.getJSON({
+            url: "../api/event/page" + page,
+            success: function (response, textStatus, jqXhr) {
+                console.log(response);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                // log the error to the console
+                console.log("The following error occured: " + textStatus, errorThrown);
+            }
+        });
+    }
+});
